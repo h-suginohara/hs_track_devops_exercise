91ad34d93330ece59ff50664cff48fafff255a65
h-suginohara <h-suginohara@tv-asahi-service.co.jp>
2026-01-21 11:36:37 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,22 @@
+def is_numeric(value):
+  return isinstance(value, (int, float)) and not isinstance(value, bool)
+
+def is_in_range(value)
+  return 0 < value <= 10
+
 def add(a, b, c=0):
+  if not is_numeric(a):
+    return (-1)
+  if not is_numeric(b):
+    return (-1)
+  if not is_numeric(c):
+    return (-1)
+  if not is_in_range(a)
+    return (-2)
+  if not is_in_range(b)
+    return (-2)
+  if not is_in_range(c)
+    return (-2)
   try:
     return int(a + b) + int(c)
   except:
